075402a5812e9290e85aeea0e8d7c46fc65496db
Katleho Ribisi <katlehoribisi@gmail.com>
2024-03-19 11:59:08 +0200

    My Jobs progression

--- src/app/api/users/register/route.ts
+++ src/app/api/users/register/route.ts
@@ -2,13 +2,12 @@
 
 // using next you can use HTTP request 
 import { connectDB } from "@/app/config/dbConfig";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 connectDB();
 
-export async function POST(request: NextRequest){
+export async function POST(request: NextRequest) {
   return NextResponse.json({
-    message: "users/register api accessed with post method"
+    // Process a POST request
+    message: "users/register api accessed with post method."
   })
 }
- 
-
